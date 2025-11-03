176ba26029553e5fd5661ea12abd01c74a86cc2c
Vitoria Almeida <almeidademariavitoria@gmail.com>
2025-11-03 16:27:18 -0300

    pula_pula 40 min fazendo o metodo leave

--- myrep/poo/pula-pula/py/draft.py
+++ myrep/poo/pula-pula/py/draft.py
@@ -1,2 +1,46 @@
 class Kid:
-    def __init
+    def __init__(self, name: str, age: int):
+        self.__name: str = name
+        self.__age: int = age
+
+    def getName(self) -> str:
+        return self.__name
+    
+    def getAge(self) -> int:
+        return self.__age
+
+    def setAge(self, age: int) -> int:
+        self.__age = age
+    
+    def setName(self, name: str) -> str:
+        self.__name = name
+    
+    def __str__(self) -> str:
+        return f"{self.__name}:{self.__age}"
+
+class pulaP:
+    def __init__(self):
+        self.__bricando: list[Kid] = []
+        self.__esperar: list[Kid] = []
+
+    #def __removeFromList(self, name: str, list: list[Kid]) -> Kid | None:
+        
+    def arrive(self, kid: Kid) -> None:
+        self.__esperar.append(kid)
+
+    def enter (self) -> None:
+        if self.__esperar:
+            Kid = self.__esperar.pop(0)
+            self.__bricando.append(Kid)
+
+    def leave(self):
+        if self.__bricando:
+            Kid = self.__esperar.pop(0)
+            self.__bricando.append(Kid)
+       
+
+
+
+
+    
+

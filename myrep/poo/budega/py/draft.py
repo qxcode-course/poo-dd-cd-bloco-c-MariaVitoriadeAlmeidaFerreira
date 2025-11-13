7ebe108a59c591804af4058d541df5aecf54b530
Vitoria Almeida <almeidademariavitoria@gmail.com>
2025-11-13 09:51:05 -0300

    budega 40 min to string

--- myrep/poo/budega/py/draft.py
+++ myrep/poo/budega/py/draft.py
@@ -1,9 +1,26 @@
 class Pessoa:
     def __init__(self, nome: str):
-        self.__nome = nome
+        self.nome = nome
 
     def __str__(self):
-        return self.__nome
+        return self.nome
 
-class Budega
+class Budega:
+    def __init__(self, num_caixa: int):
+        self.caixa: list[Pessoa | None] = [] * num_caixa
+        self.espera: list[Pessoa] = []
+
+    def __str__(self):
+        caixa = ", ". join ([str (x) for x in self.caixa])
+        espera
+        return f"{self.caixa}\n{self.espera}"
+    
+pessoa = Pessoa("Pedro")
+print(pessoa)
+
+budega = Budega(5)
+budega.caixa[2] = pessoa
+budega.espera.append(pessoa)
+print(budega) 
+
     

09aae3635646572b0526a8557fdc11610ca92324
Vitoria Almeida <almeidademariavitoria@gmail.com>
2025-11-16 18:51:52 -0300

    atividade budega terminada

--- myrep/poo/budega/py/draft.py
+++ myrep/poo/budega/py/draft.py
@@ -12,27 +12,26 @@ class Budega:
         self.espera: list[Pessoa] = []
 
     def call(self, index: int):
-        if index < 0 or index >= len(self.espera):
+        if index < 0 or index >= len(self.caixa):
             print("fail: caixa inexcistente")
             return
-        if self.espera is not None:
+        if self.caixa[index] is not None:
             print("fail: caixa ocupado")
             return
         if len(self.espera) == 0:
-            print ("fail: caixa vazio")
+            print ("fail: sem clientes")
             return
-        self.caixa[index] = self.espera[0]
-        del self.espera[0]
+        self.caixa[index] = self.espera.pop(0)
 
     def enter(self, pessoa: Pessoa):
         self.espera.append(pessoa)
     
     def finish(self, index:int):
         if index < 0 or index >= len(self.caixa):
-            print("indice ine")
+            print("fail: caixa inexistente")
             return
         if self.caixa[index] is None:
-            print("fail: caixa ocupado")
+            print("fail: caixa vazio")
         self.caixa[index] = None
 
     def giveUp(self, nome:str):
@@ -45,18 +44,30 @@ class Budega:
     def __str__(self):
         caixa = ", ".join (["-----" if x is None else str (x) for x in self.caixa])
         espera = ", ".join ([str (x) for x in self.espera])
-        return f"Caixas: [{caixa}]\nEspera: [{espera}]\n"
+        return f"Caixas: [{caixa}]\nEspera: [{espera}]"
     
 def main():
-    budega = Budega(2)
+    budega = Budega(0)
     while True:
         line: str = input()
         print("$" + line)
         args: list[str] = line.split(" ")
         if args[0] == "end":
             break
+        elif args[0] == "init":
+            num_caixa = int(args[1])
+            budega = Budega(num_caixa)
         elif args[0] == "show":
             print(budega)
-        elif args == "call":
-    
+        elif args[0] == "arrive":
+            budega.enter(Pessoa(args[1]))
+        elif args[0] == "call":
+            index = int(args[1])
+            budega.call(index)
+        elif args[0] == "finish":
+            index = int(args[1])
+            budega.finish(index)
+        elif args[0] =="Giveup":
+            budega.giveUp(Pessoa(args[1]))
+        
 main()

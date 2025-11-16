f3bc675e6ab1a84ef5b3eb0dc47f559a213fbc26
Vitoria Almeida <almeidademariavitoria@gmail.com>
2025-11-16 17:27:24 -0300

    fazendo a def giveUp

--- myrep/poo/budega/py/draft.py
+++ myrep/poo/budega/py/draft.py
@@ -9,9 +9,35 @@ class Budega:
     def __init__(self, num_caixa: int):
         self.caixa: list[Pessoa | None] = [None] * num_caixa
         self.espera: list[Pessoa] = []
-    
+
+    def call(self, index: int):
+        if index < 0 or index >= len(self.caixa):
+            print("fail: caixa inexcistente")
+            return
+        if self.caixa is not None:
+            print("fail: caixa ocupado")
+            return
+        if len(self.espera) == 0:
+            print ("fail: caixa vazio")
+            return
+        self.caixa[index] = self.espera[0]
+        del self.espera[0]
+
     def enter(self, pessoa: Pessoa):
         self.espera.append(pessoa)
+    
+    def finish(self, index:int):
+        if index < 0 or index >= len(self.caixa):
+            print("indice ine")
+            return
+        if self.caixa[index] is None:
+            print("fail: caixa ocupado")
+        self.caixa[index] = None
+
+    def giveUp(self, nome:str):
+        for i, pessoa in enumerate(self.espera):
+            
+
 
     def __str__(self):
         self.caixa =[Pessoa("Maria"), None, Pessoa("Pedro")]
@@ -23,8 +49,8 @@ pessoa = Pessoa("Pedro")
 print(pessoa)
 
 budega = Budega(5)
-budega.caixa[2] = pessoa
-budega.espera.append(pessoa)
-print(budega) 
+budega.enter(Pessoa("Vitoria"))
+budega.enter(Pessoa("Ph"))
 
+print(budega)
     

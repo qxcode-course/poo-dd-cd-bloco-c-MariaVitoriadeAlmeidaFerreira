bc9f594fb040606138de5eb27db5bfae1ed46c12
Vitoria Almeida <almeidademariavitoria@gmail.com>
2025-11-19 19:43:01 -0300

    fazendo o def procurar 70 min

--- myrep/poo/cinema/py/draft.py
+++ myrep/poo/cinema/py/draft.py
@@ -15,12 +15,39 @@ class Cliente:
     def setPhone(self, phone):
         self.phone = phone
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.id}:{self.phone}"
     
 class Teatro:
-    def __init__(self, capacidadde: int):
-        self.assentos = list[Cliente] = []
-        self.pesquisar = [name] * int
-    
+    def __init__(self, capacidade: int):
+        self.assentos: list[Cliente | None] = [None] * capacidade
+        self.capacidade: int = capacidade
 
+    def __str__(self) -> str:
+        return f"{self.assentos}"
+
+    def procurar(self, nome_cliente: str) -> int:
+        for i, cliente in enumerate(self.assentos):
+            if cliente is not None and cliente.getId() == nome_cliente:
+                return i
+            return -1 # verica depois de tudo
+
+
+
+teatro = Teatro(5)
+print(teatro.assentos)
+
+
+
+
+#def main():
+   # teatro = Teatro()
+  #  while True:
+   #     line: str = input()
+    #    print("$" + line)
+     #   args: list[str] = line.split(" ")
+      #  if args[0] == "end":
+       #     break
+        #elif args[0] == "show":
+         #   print(teatro)
+#main()
